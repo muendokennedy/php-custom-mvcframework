2ef6d220f3e689d2dc1e71c4aff336a18c97a647
muendokennedy <kennedymunyao999@gmail.com>
2023-06-26 12:03:27 +0300

    Established the PDO database connection

--- core/Database.php
+++ core/Database.php
@@ -10,8 +10,11 @@ class Database
     $dsn = $config['dsn'] ?? '';
     $user = $config['user'] ?? '';
     $password = $config['password'] ?? '';
+
     $this->pdo = new \PDO($dsn, $user, $password);
     $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
   }
+
+
   
 }
